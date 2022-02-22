fb6d31f43d8253793f7740e2b7c22993b295d7ea
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-02-22 07:43:46 +0200

    cfg
    Created a cfg file with regex.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
-const fs = require('fs');
-const path = require('path');
+import cfg from './utils/cfg';
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('idmentor.IDMentor', async function () {
 		const editor = vscode.window.activeTextEditor;
@@ -10,10 +9,10 @@ export function activate(context: vscode.ExtensionContext) {
 			const selection: vscode.Selection = editor.selection;
 
 			// Get the word within the selection
-			const data: string = document.getText(selection);
-			let result: string[] = data.match(/id="[A-za-z0-9-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]+"/g).join('').match(/\".*?\"/g).join(' ').split(' ');
-			let copy: string = result.map(el => `let ${el.replace('"', '').slice(0, el.length - 2)} = document.getElementById(${el});\n`).join('');
-			vscode.env.clipboard.writeText(copy);
+			const documentTextSelection: string = document.getText(selection);
+			const documentIds: string[] = documentTextSelection.match(cfg.id).join('').match(/\".*?\"/g).join(' ').split(' ');
+			const idMentor: string = documentIds.map(el => `let ${el.replace('"', '').slice(0, el.length - 2)} = document.getElementById(${el});\n`).join('');
+			vscode.env.clipboard.writeText(idMentor);
 		}
 	});
 	context.subscriptions.push(disposable);
