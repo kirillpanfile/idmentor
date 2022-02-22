2493c81664626d51dfa6d406c6f2d8f871e27785
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-02-22 10:36:54 +0200

    bug fixes
    Fixed error with null elements.

--- src/utils/textBuilder.ts
+++ src/utils/textBuilder.ts
@@ -3,13 +3,13 @@ import cfg from './cfg';
 const toFindDuplicates = (arr: string[]): string[] => arr.filter((item: string, index: number) => arr.indexOf(item) !== index);
 const clear = (el: string): string => el.replace('"', '').slice(0, el.length - 2);
 export function getID(documentTextSelection: string): string {
-    let tempData: string[] = documentTextSelection
-        .match(cfg.id)
-        .join('')
-        .match(cfg.idJunk)
-        .join(' ')
-        .split(' ');
-    if (tempData.length > 0) {
+    let tempData: string[] = documentTextSelection.match(cfg.id)
+    if (tempData) {
+        tempData = tempData
+            .join('')
+            .match(cfg.idJunk)
+            .join(' ')
+            .split(' ');
         if (toFindDuplicates(tempData).length === 0) {
             return tempData
                 .map(el => `const ${el
@@ -29,29 +29,29 @@ export function getID(documentTextSelection: string): string {
     return '';
 }
 
-// export function getClass(documentTextSelection: string): string {
-//     let tempData: string[] = documentTextSelection
-//         .match(cfg.class)
-//         .join('')
-//         .match(cfg.idJunk)
-//         .join(' ')
-//         .split(' ');
-//     if (tempData.length > 0) {
-//         if (toFindDuplicates(tempData).length === 0) {
-//             return tempData
-//                 .map(el => `const ${el
-//                     .replace('"', '')
-//                     .slice(0, el.length - 2)} = document.querySelector(${el});\n`)
-//                 .join('');
-//         }
-//         else {
-//             const duplicateData: string[] = toFindDuplicates(tempData);
-//             tempData = [...new Set(tempData)];
-//             return tempData.map(el => {
-//                 return duplicateData.includes(el) ? `const ${clear(el)} = document.querySelectorAll(".${clear(el)}");\n` :
-//                     `const ${clear(el)} = document.querySelector(${el});\n`;
-//             }).join('');
-//         }
-//     }
-//     return '';
-// }
+export function getClass(documentTextSelection: string): string {
+    let tempData: string[] = documentTextSelection.match(cfg.class)
+    if (tempData) {
+        tempData = tempData
+            .join('')
+            .match(cfg.idJunk)
+            .join(' ')
+            .split(' ');
+        if (toFindDuplicates(tempData).length === 0) {
+            return tempData
+                .map(el => `const ${el
+                    .replace('"', '')
+                    .slice(0, el.length - 2)} = document.querySelector(${el});\n`)
+                .join('');
+        }
+        else {
+            const duplicateData: string[] = toFindDuplicates(tempData);
+            tempData = [...new Set(tempData)];
+            return tempData.map(el => {
+                return duplicateData.includes(el) ? `const ${clear(el)} = document.querySelectorAll(".${clear(el)}");\n` :
+                    `const ${clear(el)} = document.querySelector(${el});\n`;
+            }).join('');
+        }
+    }
+    return '';
+}
